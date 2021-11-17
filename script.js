f6e65e05771b85ad4f6a0e34ededf097b2bcae41
ritu <ritu.kumari@nagarro.com>
2021-11-17 19:14:22 +0530

    simple commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let SearchBtn = document.querySelector('#search-btn');
+let SearchBar = document.querySelector('.search-container');
+
+SearchBtn.addEventListener('click',() =>{
+
+SearchBtn.classList.toggle('fa-times')
+});
